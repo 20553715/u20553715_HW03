8becc44d731d07a5d5644a3e14a94b23b937898c
20553715 <u20553715@tuks.co.za>
2022-06-01 19:50:37 +0200

    IndexPage

---
+++ Models/FileModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations; //Add for look and feel
+using System.Web;
+
+namespace u20553715_HW03.Models
+{
+    public class FileModel
+    {
+        //Display options.
+
+        [Display(Name = "File Name")]
+        public string FileName { get; set; }
+
+        [Required(ErrorMessage = "Please select file.")]
+        [Display(Name = "Browse File")]
+        public HttpPostedFileBase[] Files { get; set; }
+    }
+}
