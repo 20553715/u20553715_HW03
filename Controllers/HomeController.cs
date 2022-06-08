90f4483fa78fe554227b59023783dbbb7a1ec325
20553715 <u20553715@tuks.co.za>
2022-06-08 17:17:19 +0200

    Documents Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,28 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace u20553715_HW03.Controllers
 {
     public class HomeController : Controller
     {
+        //GET: Index
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        //POST: Index
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase files, string File) //INSIDE HOME
+        {
+           
+
+            if (files != null && files.ContentLength > 0 && File == "Document")
+            {
+
+                var fileName = Path.GetFileName(files.FileName);
+                var path = Path.Combine(Server.MapPath("~/Media/Documents/"), fileName);
+
+                files.SaveAs(path);
+
+            }
+            if (files != null && files.ContentLength > 0 && File == "Image")
+            {
+
+                var fileName = Path.GetFileName(files.FileName);
+                var path = Path.Combine(Server.MapPath("~/Media/Images"), fileName);
+
+                files.SaveAs(path);
+
+            }
+            if (files != null && files.ContentLength > 0 && File == "Video")
+            {
+
+                var fileName = Path.GetFileName(files.FileName);
+                var path = Path.Combine(Server.MapPath("~/Media/Video"), fileName);
+
+                files.SaveAs(path);
+
+            }
+            // redirect back to the index action to show the form once again
+
+            return RedirectToAction("Index");
+        }
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Trial()
-        {
-            ViewBag.Message = "Your contact page.";
+        
 
-            return View();
-        }
     }
 }
